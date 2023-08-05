c0d7f8edbe3267771e2f4235c687b7654d35c54e
saher <murtazasaher@gmail.com>
2023-08-04 19:04:25 -0700

    added email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,31 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'randomrandom123d@gmail.com', # Replace with your Gmail email address
+    password:             'zvsgaovxsterorbg',        # Replace with your Gmail password
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+  
+  # ActionMailer::Base.delivery_method = :smtp
+  # ActionMailer::Base.smtp_settings = {
+  #   :tls => true,
+  #   :address => "smtp.gmail.com",
+  #   :port => 587,
+  #   :domain => "gmail.com",
+  #   :authentication => :login,
+  #   :user_name => "randomrandom123d@gmail.com",
+  #   :password => "pasword123d"
+  # }
+  
+  
+  
+
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :accounts
+
+  resources :posts do
+    resources :comments
+  end
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
